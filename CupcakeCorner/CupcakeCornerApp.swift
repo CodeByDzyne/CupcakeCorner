f8a062c3499ddbabc13b2890fdfd96aa029f9caa
CodeByDzyne <wdenley@dzynesolutions.com>
2022-12-08 22:11:45 +1100

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Wealthy Waz on 8/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
